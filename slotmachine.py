a0b0850d2b32ee03bedb8caec43f8eff32663fa1
Nagalie <151873220+Nagalie@users.noreply.github.com>
2026-01-22 09:30:00 +0100

    Datei Hochgeladen

---
+++ slotmachine.py
@@ -0,0 +1,34 @@
+import random
+
+#main 
+def main(): 
+
+    symbols = ["%", "§", "&", "7", "v"]
+    weights = [40, 30, 20, 9, 1]
+
+    geld = int(0);
+    #int(geldeinzahlen);
+
+    print("SLOTMACHINE")
+
+    #print("Wollen Sie frei gamblen (1) oder einzahlen (0)?")
+    entscheidung = bool(input("Wollen Sie frei gamblen (1) oder einzahlen (0)? "))
+
+    if entscheidung:
+        einzahlen(geld);
+    
+    else:
+        freiGamblen(geld);
+
+    
+def einzahlen(geld):
+    #print("Wie viel Geld wollen Sie einzahlen?")
+    geldeinzahlen = int(input("Wie viel Geld wollen Sie einzahlen? "))
+    geld = geld + geldeinzahlen;
+
+def freiGamblen():
+    geld = geld + 150000;
+
+
+#main ausgabe
+main();
